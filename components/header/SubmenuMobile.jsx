2dc9dfa00d47d5b68e9594b2b82c630bd0a64a3f
Newton Figueiredo <new.figueiredo@gmail.com>
2022-03-13 12:36:52 -0300

    concluindo navbar v2

--- components/header/SubmenuMobile.jsx
+++ components/header/SubmenuMobile.jsx
@@ -3,44 +3,44 @@ export default function SubmenuMobile(props) {
 
     return (
         <>
-            <div className={`submenu-container flex flex-col lg:hidden absolute ${props.click ? "left-0" : "-left-full"} ease-out duration-300 top-[102px] bg-white z-50 w-full`} >
+            <div className={`submenu-container flex flex-col lg:hidden absolute ${props.click ? "left-0" : "-left-full"} ease-out duration-300 top-[104px] bg-white z-50 w-full shadow`} >
                 <div className="submenu-content flex flex-col">
-                    <a  href="/" className={`flex justify-center items-center flex-row py-2`}>
-                        <span className='lg:p-2'>
+                    <a href="/" className={`flex justify-center items-center flex-row py-2`}>
+                        <span className='  text-[#FAA500] font-semibold'>
                             Novidades
                         </span>
                     </a>
 
-                    <a  href="/" className={`flex justify-center items-center flex-row py-2`}>
-                        <span className='lg:p-2'>
+                    <a href="/" className={`flex justify-center items-center flex-row py-2`}>
+                        <span className=''>
                             Vestidos
                         </span>
                     </a>
-                    <a  href="/" className={`flex justify-center items-center flex-row py-2`}>
-                        <span className='lg:p-2'>
+                    <a href="/" className={`flex justify-center items-center flex-row py-2`}>
+                        <span className=''>
                             Roupas
                         </span>
                     </a>
 
-                    <a  href="/" className={`flex justify-center items-center flex-row py-2`}>
-                        <span className='lg:p-2'>
+                    <a href="/" className={`flex justify-center items-center flex-row py-2`}>
+                        <span className=''>
                             Sapatos
                         </span>
                     </a>
-                    <a  href="/" className={`flex justify-center items-center flex-row py-2`}>
-                        <span className='lg:p-2'>
+                    <a href="/" className={`flex justify-center items-center flex-row py-2`}>
+                        <span className=''>
                             Lingerie
                         </span>
                     </a>
 
-                    <a  href="" className={`flex justify-center items-center flex-row py-2`}>
-                        <span className='lg:p-2'>
+                    <a href="" className={`flex justify-center items-center flex-row py-2`}>
+                        <span className=''>
                             Acessórios
                         </span>
                     </a>
 
-                    <a  href="" className={`flex justify-center items-center flex-row py-2`}>
-                        <span className='lg:p-2'>
+                    <a href="" className={`flex justify-center items-center flex-row py-2`}>
+                        <span className=''>
                             OUTLET
                         </span>
                     </a>
